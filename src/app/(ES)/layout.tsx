10cfc228117c6d1ecbd313292e1b58a41ffbb951
Vincent Botoku <desarrollador@grupolvt.com>
2025-08-19 17:34:11 -0600

    init

--- src/app/(ES)/layout.tsx
+++ src/app/(ES)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import {  Zilla_Slab } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/generalUI/Header";
 import FloatingMenu from "@/components/generalUI/FloatingMenu";
 
